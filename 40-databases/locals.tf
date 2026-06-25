3a5a522932a2b215e0b8652cda436bdaed1876e3
haroon <haroonbashamohammad@gmail.com>
2026-06-25 21:37:50 +0530

    terraform

---
+++ 40-databases/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
+  mongodb_sg_id      = data.aws_ssm_parameter.mongodb_sg_id.value
+  database_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  ami_id = ami-0220d79f3f480ecf5
+  common_tags = {
+    project     = var.project_name
+    environment = var.environment
+    terraform   = true
+    
+  }
+}
+
+
+
