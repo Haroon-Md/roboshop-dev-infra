e97ce94720e6d645ec968cf6b9c70cc06eba40a0
haroon <haroonbashamohammad@gmail.com>
2026-06-27 11:09:48 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -27,3 +27,14 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   to_port           = 22
 
 }
+
+
+resource "aws_security_group_rule" "redis_bastion" {
+  security_group_id = local.redis_sg_id 
+  source_security_group_id = local.bastion_sg_id
+  type              = "ingress"
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+
+}
