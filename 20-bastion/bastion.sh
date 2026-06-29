be43a2795cd682ccf46ce8a7fcbb362ca623154a
haroon <haroonbashamohammad@gmail.com>
2026-06-29 15:54:04 +0530

    modified

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -3,3 +3,4 @@
 sudo yum install -y yum-utilis
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform 
+sudo dnf install -y ansible python3-boto3 python3-botocore
