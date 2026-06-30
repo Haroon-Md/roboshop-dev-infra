5b1b79c5fb06d265995038da08cef263d3683505
haroon <haroonbashamohammad@gmail.com>
2026-06-30 16:16:39 +0530

    modified

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -43,7 +43,7 @@ resource "terraform_data" "catalogue" {
     inline = [
       "chmod +x /tmp/catalogue.sh",
       #"sudo sh /tmp/catalogue.sh "
-      "sudo sh /tmp/catalogue.sh catalogue"
+      "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
     ]
   }
 
