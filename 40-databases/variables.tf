e526f46bdf4a461b6ce37d3566f4ab958d7132cb
haroon <haroonbashamohammad@gmail.com>
2026-06-30 13:04:58 +0530

    modified

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,29 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "sg_names" {
+  default = [
+    #databases
+    "mongodb", "redis", "mysql", "rabbitmq",
+    #backend
+    "catalogue", "user", "cart", "shipping", "payment",
+    #frontend
+    "frontend",
+    #bastion
+    "bastion",
+    #load balencer frontend
+    "frontend_alb",
+    #backend alb
+    "backend_alb"
+
+  ]
+}
+
+variable "zone_id"{
+  default = Z024817821F8L6OR4TDA8
+}
+
+variable "domain_name" {
+  default = ahay.space
+}
