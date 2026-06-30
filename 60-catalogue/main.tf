958b55c4fb2f7b16ced0ff47a67477cb94c2c495
haroon <haroonbashamohammad@gmail.com>
2026-06-30 13:21:56 +0530

    modified

---
+++ 60-catalogue/main.tf
@@ -0,0 +1,52 @@
+#create Ec2 instance 
+
+resource "aws_instance" "catalogue" {
+  ami = local.ami_id
+  #  "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-catalogue" #roboshop-dev-catalogue
+    }
+
+  )
+
+}
+
+# connect to instance using remote-exec privisioner through terraform_data 
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+
+  }
+
+  #terraform copies this file to catalogue server
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      #"sudo sh /tmp/catalogue.sh "
+      "sudo sh /tmp/catalogue.sh catalogue"
+    ]
+  }
+
+}
+
+
