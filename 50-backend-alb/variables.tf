7cc2834eacb5a695fa594ee20b9e209bf2e4126b
haroon <haroonbashamohammad@gmail.com>
2026-07-01 12:33:51 +0530

    modified

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "domain_name" {
+  default = "ahay.space"
+}
+
+variable "zone_id"{
+  default = "Z024817821F8L6OR4TDA8"
+}
