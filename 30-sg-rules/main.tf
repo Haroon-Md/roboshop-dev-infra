ab74f714d581eb7ed07af183c9713013eba8a04a
haroon <haroonbashamohammad@gmail.com>
2026-07-01 22:12:17 +0530

    modified

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -93,3 +93,13 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   to_port                  = 8080
 
 }
+
+resource "aws_security_group_rule" "frontend_alb_public" {
+  security_group_id = local.frontend_alb_sg_id
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+
+}
