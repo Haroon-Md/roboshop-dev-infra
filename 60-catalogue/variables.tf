23ca22980908cfc1fd124e0232fd885a071b2aa1
haroon <haroonbashamohammad@gmail.com>
2026-07-01 12:56:57 +0530

    modified

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id"{
+  default = "Z024817821F8L6OR4TDA8"
+}
+
+variable "domain_name" {
+  default = "ahay.space"
+}
