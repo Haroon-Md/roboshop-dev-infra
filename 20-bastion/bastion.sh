17c34c85f67db5ff2efe3b2415593aee1b3113fe
haroon <haroonbashamohammad@gmail.com>
2026-07-01 08:14:58 +0530

    modified

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#growing the /home volume size to 30GB
+sudo growpart /dev/nvme0n1 4
+lvextend -L 30G /dev/RootVG/rootVol
+xfs_growfs /home 
+
 sudo yum install -y yum-utilis
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform 
